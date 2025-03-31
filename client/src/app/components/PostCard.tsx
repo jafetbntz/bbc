c2dbb8f6ac94b9591ed42adf6519e81f4662d5c3
Jafet Benítez <jafetg.benitez@gmail.com>
2025-03-31 11:38:06 -0400

    [Feature]: Install date-fns

--- client/src/app/components/PostCard.tsx
+++ client/src/app/components/PostCard.tsx
@@ -1,16 +1,23 @@
 import Link from "next/link";
 import { IPost } from "../model/post.interface";
+import { formatDistance } from "date-fns";
 
 export type PostCardPropsProps = {
     post: IPost
 }
   
   export default function PostCardProps( {post}: PostCardPropsProps) {
-
+        const publishingDate = post.createDate ? formatDistance(post.createDate, new Date(), { addSuffix: true }): null;
         return (
-            <div className="card bg-base-100 card-xs shadow-sm">
+            <div className="card bg-base-100 card-xs shadow-sm p-1">
             <div className="card-body">
               <h2 className="card-title">{post.title}</h2>
+              {
+                              (
+                                publishingDate &&
+                                <p className="text-gray italic">{publishingDate}</p>
+                              )
+              }
               <p>
                 {post.content}
               </p>
@@ -18,8 +25,11 @@ export type PostCardPropsProps = {
                 <Link href={`/posts/${post.id}`} className="btn btn-ghost">Read more</Link>
 
                 {(post.slug && <Link href={`/live/${post.slug}`} className="btn btn-primary">Go live</Link>)}
+                
               </div>
             </div>
+
+
           </div>
         );
 
